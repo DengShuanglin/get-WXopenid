03d7350392d51449471d84e48533fdcc0eaea24a
Deng Shuanglin <shuanglindev@gmail.com>
2022-01-07 16:29:31 +0800

    feat: 添加token api

--- app.js
+++ app.js
@@ -14,10 +14,19 @@ app.use("/login", (req, res) => {
 
   request(url, (err, response, body) => {
     console.log("body", body);
-    res.send(body); //将请求到的 OpenID与 session_key 返回给小程序页面js文件
+    res.send(body);
+  });
+});
+
+app.use("/token", (req, res) => {
+  const url = `https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=${appid}&secret=${appsecret}`;
+
+  request(url, (err, response, body) => {
+    console.log("body", body);
+    res.send(body);
   });
 });
 
 app.listen(8000, () => {
-  console.log("服务启动");
+  console.log("服务启动, 端口号: 8000");
 });
